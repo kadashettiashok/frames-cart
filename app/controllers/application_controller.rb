975bc66bde148d5190b77ce61311ec3afe8afc5d
kadashettiashok <ashok.kadashetti@softsuave.com>
2022-03-02 17:57:23 +0530

    add exception handling for enum attribute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
     before_action :set_user, only: %i[index create]
+    rescue_from ArgumentError, with: :render_unprocessable_entity_response
+
     def set_user
       user = User.find_by(authentication_token: request.headers['token'])
       if user.present?
@@ -13,5 +15,12 @@ class ApplicationController < ActionController::API
   
     def current_user
       @current_user
+    end
+
+    private
+    
+    def render_unprocessable_entity_response(invalid)
+        byebug
+      render json: { errors: invalid.message }, status: :unprocessable_entity
     end
   end
