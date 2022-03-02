34ec94823823c9a6193a41c93e093cb4db956cc2
kadashettiashok <ashok.kadashetti@softsuave.com>
2022-03-02 17:59:24 +0530

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |user|
+    User.create!(
+      name: "softsuave #{user}",
+      email: "softsuave#{user}@gmail.com",
+      password: "123456",
+      role: 'user',
+      currency: "USD"
+    )
+  end
+  puts '3 Users created!'
+  
+
+
+  3.times do |frame|
+    Frame.create!(
+      name: "frame #{frame}",
+      description: "description#{proj}",
+      status: "active",
+      stock: 10,
+      price: 20
+    )
+  end
+  puts '3 Frames created!'
+
+
+
+  3.times do |lens|
+    Frame.create!(
+      name: "lens #{lens}",
+      description: "description",
+      prescription_type: "fashion",
+      lens_type: "classic"
+      stock: 15,
+      price: 30
+    )
+  end
+  puts '3 Frames created!'
+
+  
+
+
