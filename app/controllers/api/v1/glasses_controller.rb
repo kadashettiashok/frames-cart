fb3f4e49b1fb35b73287f9ec27d2ef7ae87d61e1
kadashettiashok <ashok.kadashetti@softsuave.com>
2022-03-02 19:23:26 +0530

    add currency converter

--- app/controllers/api/v1/glasses_controller.rb
+++ app/controllers/api/v1/glasses_controller.rb
@@ -22,7 +22,7 @@ module Api
           total_price = frame.price + lense.price
           Frame.find_by(id: frame.id).update(stock: (frame.stock - 1))
           Lense.find_by(id: lense.id).update(stock: (lense.stock - 1))
-          render json: { price: Services::CovertCurrencyService.new(current_user.currency, total_price).call }
+          render json: { price: currency_converter(current_user.currency, total_price) }
         end
       end
 
@@ -30,6 +30,23 @@ module Api
 
       def glass_params
         params.permit(:frame_id, :lense_id)
+      end
+      
+      def currency_converter(type, sum)
+        case type
+        when 'USD'
+          "#{(sum * 0.013).round(3)} Doller"
+        when 'EUR'
+          "#{(sum * 0.012).round(3)}  Euro"
+        when 'GBP'
+          "#{(sum * 0.0099).round(3)}  Pound"
+        when 'JOD'
+          "#{(sum * 0.0094).round(3)}  Jordanian Dinar"
+        when 'JPY'
+          "#{(sum * 1.52).round(3)}  Japanese Yen"
+        else
+          "#{sum} Rupees"
+        end
       end
     end
   end
