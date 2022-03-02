3d51a957b419a11e93e9bd548960b13a24d0643f
kadashettiashok <ashok.kadashetti@softsuave.com>
2022-03-02 15:05:28 +0530

    add enum

--- app/models/lense.rb
+++ app/models/lense.rb
@@ -1,2 +1,4 @@
 class Lense < ApplicationRecord
+    enum prescription_type: {fashion: "fashion",  single_vision: "single_vision", varifocals:"varifocals"}
+    enum lens_type: {classic: "classic", blue_light: "blue_light", transition: "blue_light"}
 end
